e04ffe3b0878de0bb687c0caf08eb0c72d370586
Gonzalo Pardo <gonzalopardo@Gonzalos-MacBook-Pro.local>
2024-04-17 14:13:20 +0200

    feat: add some missing doc

--- src/main/kotlin/com/seatcode/seatroboticmowercontrol/application/MowingWorkflowUseCase.kt
+++ src/main/kotlin/com/seatcode/seatroboticmowercontrol/application/MowingWorkflowUseCase.kt
@@ -8,6 +8,9 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
+/**
+ * MowingWorkflow Port implementation.
+ */
 @Service
 class MowingWorkflowUseCase(
     private val parser: MultipartFileParser,
--- src/main/kotlin/com/seatcode/seatroboticmowercontrol/application/error/MowerCommandException.kt
+++ src/main/kotlin/com/seatcode/seatroboticmowercontrol/application/error/MowerCommandException.kt
@@ -1,3 +1,6 @@
 package com.seatcode.seatroboticmowercontrol.application.error
 
+/**
+ * Custom exception to be thrown on mover command execution errors
+ */
 class MowerCommandException(message: String) : RuntimeException(message)
